9ee028e6e166eef64c4f9740152e7f6cbff31157
Ryan-Houpt <rehoupt@gmail.com>
2018-10-10 15:20:53 -0500

    array exercsies

--- js/iterating.js
+++ js/iterating.js
@@ -50,7 +50,7 @@ for (var i = 0; i < people.length; i++) {
      * Refactor your above code to use a `forEach` loop
      */
 
-    people.forEach(function(element, index, array) {
+    people.forEach(function(element) {
         console.log(element)
     });
 
@@ -88,3 +88,65 @@ secondItem(people);
 
 lastItem(people);
 })();
+
+
+// ================================= ARRAY BONUSES
+// 1. Create a function that returns a random day of the week
+
+var randomDay = Math.ceil(Math.random() * 6);
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+console.log(days[randomDay]);
+
+
+// 2. Create a function that takes a single letter and returns what number the letter is in the alphabet. Ignore case.
+// someFunction("a") // returns 1
+// someFunction("z") // returns 26
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+function alphabet (input) {
+        return (letters.indexOf(input) + 1);
+}
+
+console.log(alphabet("a"));
+
+// 3. Create a function that returns the longest string in a given array of string elements
+
+var array = ['first item', 'second item is longer than the third one',
+    'third longish item', 'but this one is most definetly the longest because I said so, so there it is'];
+
+var length = 0;
+var longest;
+
+for(var i=0; i < array.length; i++){
+    if(array[i].length > length){
+        var length = array[i].length;
+        longest = array[i];
+    }
+}
+
+console.log(longest);
+
+// 4. Create a function that takes in two arrays of number inputs. If all numbers added together in the first array is equal to
+// all the number inputs added in the second array, return true, otherwise, false. Only add together numeric elements but either array
+// may contain non-numeric elements.
+//     var arr1 = ['bob', 1, true, 1, 2];
+// var arr2 = [2, null, undefined, 0, 2, "apple"]
+// exampleFunction(arr1, arr2) // returns true
+
+
+var array1 = [1, 2, 3, 8, 10].reduce((a, b) => a + b, 0);
+var array2 = [1, 2, 3, 8, 10].reduce((a, b) => a + b, 0);
+
+function compareArrays(array1, array2) {
+     if (array1 === array2) {
+         return true;
+     }  else
+         return false;
+}
+
+console.log(compareArrays(array1, array2));
+
+
