3aa311e3fe36447550e9d4572b13bb280c6a580f
Ryan-Houpt <rehoupt@gmail.com>
2018-10-25 15:58:57 -0500

    adding json exercises

---
+++ js/pre-ajax.js
@@ -0,0 +1,14 @@
+var people = [
+    {
+        name:"Fred",
+        favColor: "red"
+    },
+    {
+        name: "Sally",
+        favColor: "blue"
+    },
+    {
+        name: "Tom",
+        favColor: "yellow"
+    }
+];
